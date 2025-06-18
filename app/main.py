fdcc39f4d860e5940ce1d68f9e31142b5a01ec4e
Vovas <vovaafon25@gmail.com>
2025-06-18 20:30:52 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,28 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    _cache_dict = {}
+
+    @wraps(func)
+    def wrapper(*args: tuple) -> int:
+        if func.__name__ in _cache_dict:
+            if args in _cache_dict[func.__name__]:
+                print("Getting from cache")
+                return _cache_dict[func.__name__][args]
+
+        for arg in args:
+            if not isinstance(arg, (int, float, bool, str, tuple)):
+                return func(*args)
+
+        print("Calculating new result")
+        result = func(*args)
+
+        if func.__name__ not in _cache_dict:
+            _cache_dict[func.__name__] = {}
+
+        _cache_dict[func.__name__][args] = result
+
+        return result
+    return wrapper
